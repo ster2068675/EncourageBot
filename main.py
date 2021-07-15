2b89366ce375102627910487df88155203dca5cd
Sterling <57185391+ster2068675@users.noreply.github.com>
2021-07-15 03:42:06 +0000

    Removed old, synchronous requests to async aiohttp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 import os
 #allows code to make https requests to retrieve data from api
-#import requests
 import aiohttp
 #api returns JSON
 import json
@@ -17,24 +16,18 @@ init_encouragements = [
     "Sorry you felt that way~", "*sends you a virtual hug*", "<3"
 ]
 
-
-#def get_quote():
-#    response = requests.get("https://zenquotes.io/api/random")
-#    json_data = json.loads(
-#        response.text)  #getting the data from the json returned
-#    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-#    return (quote)
-
+if "ready" not in db.keys():
+  db["ready"] = True
 
 def update_encouragements(enc_msg):
     if "encouragements" in db.keys():
         encouragements = db["encouragements"]
         encouragements.append(enc_msg)
-        print(enc_msg + ' was added to the database!')
+        print(enc_msg + ' was added to the eng_msg database!')
         db["encouragements"] = encouragements
     else:
-        db["encouragments"] = [enc_msg]
-
+        db["encouragements"] = [enc_msg]
+        print(enc_msg + ' was added to the new eng_msg database!')
 
 def delete_encouragement(index):
     encouragements = db["encouragements"]
@@ -42,11 +35,9 @@ def delete_encouragement(index):
         del encouragements[index]
         db["encouragements"] = encouragements
 
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 
 @client.event
 async def on_message(message):
@@ -67,27 +58,45 @@ async def on_message(message):
         await message.channel.send(quote)
 
     #to combine init msgs with user submitted ones
-    options = init_encouragements
-    if "encouragements" in db.keys():
-        options = options + db["encouragements"]
+    if db["ready"]:
+      options = init_encouragements
+      if "encouragements" in db.keys():
+        options = options + list(db["encouragements"])
+        #moved this part into the conditional to avoid unbound local error
+        if any(word in msg for word in sad_words):
+          await message.channel.send(random.choice(options))
 
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(options))
 
     if msg.startswith("!new"):
         enc_msg = msg.split("!new ", 1)[1]
         update_encouragements(enc_msg)
         await message.channel.send(
             "New encouraging message added. Thanks for sharing! ^~^")
+        print('testing?')
 
     if msg.startswith("!del"):
-        encouragements = []
-        if "encouragements" in db.keys():
-            index = int(msg.split("!del", 1)[1])
-            delete_encouragement(index)
-            encouragements = db["encouragements"]
-        await message.channel.send(encouragements)
+      encouragements = []
+      if "encouragements" in db.keys():
+          index = int(msg.split("!del", 1)[1])
+          delete_encouragement(index)
+          encouragements = db["encouragements"]
+      await message.channel.send(encouragements)
 
+    if msg.startswith("!list"):
+      encouragements = []
+      if "encouragements" in db.keys():
+        encouragements = db["encouragements"]
+      await message.channel.send(encouragements)
+
+    if msg.startswith("!ready"):
+      value = msg.split("!ready ", 1)[1]
+
+      if value.lower() == "true":
+        db["ready"] = True
+        await message.channel.send("I'm ready to encourage!")
+      else:
+        db["ready"] = False
+        await message.channel.send("I'm out of here!")
 
 #dont give away tokens to strangers lol
 client.run(os.environ['TOKEN'])
